11bd5cb6ad71391cbf81caa990ae27bc998bd7d3
Vishal Chaudhary <vishal38785@gmail.com>
2023-01-25 07:21:18 +0530

    Updates

---
+++ precache-manifest.a6a654a709b1fce7ac49be141f74664e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0067e5b1a0d1ecbfd057a8b03494d5ab",
+    "url": "/Dashboard2d/index.html"
+  },
+  {
+    "revision": "b7dab72f1b1015c02c9f",
+    "url": "/Dashboard2d/static/css/main.9eddbbcd.chunk.css"
+  },
+  {
+    "revision": "4d1a8c79c7104dda73ff",
+    "url": "/Dashboard2d/static/js/2.63e6b541.chunk.js"
+  },
+  {
+    "revision": "b7dab72f1b1015c02c9f",
+    "url": "/Dashboard2d/static/js/main.c002c252.chunk.js"
+  },
+  {
+    "revision": "c8c641a711f7ef1d7aa6",
+    "url": "/Dashboard2d/static/js/runtime~main.062907ce.js"
+  },
+  {
+    "revision": "32bd88fb6ce6fa22d993a85d533a585c",
+    "url": "/Dashboard2d/static/media/tower.32bd88fb.svg"
+  }
+]);
